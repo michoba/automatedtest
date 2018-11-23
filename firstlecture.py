a2ad5eace7e51da4aa5b8999ec5e29a72719bf6e
michoba <45298828+michoba@users.noreply.github.com>
2018-11-23 21:06:27 +0000

    created first code

---
+++ firstlecture.py
@@ -0,0 +1,11 @@
+name = "Lucy"
+
+if name is "Jake":
+    print("Hey there Jake")
+    print("Firsts line")
+if name is "Lucy":
+    print("What up Lucedoggy?")
+if name is "Samy":
+    print("What up Slammy?")
+else:
+    print("Please register up for the site")
